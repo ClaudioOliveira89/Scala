e8b4fb1d4d77e0f48938c8258e5a56447ed90420
ClaudioOliveira89 <claudio.h.oliveira.s@gmail.com>
2023-09-26 19:02:11 -0300

    primeiro codigo

---
+++ Programação Básica de Introdução/Introdução.scala
@@ -0,0 +1,10 @@
+// introdução
+
+println("hello, world!")
+
+println("welcome to the world of programming")
+
+println(1) //1
+println(1 + 1) //2
+println("Hello") //Hello!
+println("Hello, " + "World!") // Hello, world!
